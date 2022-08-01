c87acc31f890c2face0c426262fd1f1557a1e615
SpetsnazSicario <rorypearson2003@hotmail.com>
2022-08-01 23:55:59 +0100

    prettier

--- App.js
+++ App.js
@@ -2,20 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
 
-const TaskCollection = 
-[
+const TaskCollection = [
     {
         id: 1,
         title: 'Sleep',
-        completed: false
+        completed: false,
     },
     {
         id: 2,
         title: 'Code',
-        completed: false
+        completed: false,
     },
-
-]
+];
 
 export default function App() {
     const [tasks, setTasks] = useState(TaskCollection);
@@ -27,33 +25,54 @@ export default function App() {
                     <Text style={styles.header_title}>Task list</Text>
                 </View>
                 <View style={styles.tasklist_body}>
-                    <Pressable style={styles.button} onPress={() => {
-                        setTasks([...tasks, {
-                            id: tasks.length + 1,
-                            title: 'New task',
-                            completed: false
-                        }])
-                    }}>
+                    <Pressable
+                        style={styles.button}
+                        onPress={() => {
+                            setTasks([
+                                ...tasks,
+                                {
+                                    id: tasks.length + 1,
+                                    title: 'New task',
+                                    completed: false,
+                                },
+                            ]);
+                        }}
+                    >
                         <Text style={styles.text}>Add Task</Text>
                     </Pressable>
-                    
-                    {TaskCollection.map(task => (
+
+                    {TaskCollection.map((task) => (
                         <View key={task.id} style={styles.task_container}>
                             <View style={styles.task_data}>
-                                <Text style={styles.task_title}>{task.title}</Text>
-                                <Text style={task.completed ? styles.task_completed : styles.task_notCompleted}>{task.completed ? "Completed" : "Not Completed"}</Text>
+                                <Text style={styles.task_title}>
+                                    {task.title}
+                                </Text>
+                                <Text
+                                    style={
+                                        task.completed
+                                            ? styles.task_completed
+                                            : styles.task_notCompleted
+                                    }
+                                >
+                                    {task.completed
+                                        ? 'Completed'
+                                        : 'Not Completed'}
+                                </Text>
                             </View>
                             {task.completed === false && (
                                 <View style={styles.task_action}>
-                                    <Pressable style={styles.tickBox} onPress={() => {
-                                        const newTasks = tasks.map(t => {
-                                            if(t.id === task.id) {
-                                                t.completed = true;
-                                            }
-                                            return t;
-                                        })
-                                        setTasks(newTasks);
-                                    }}>
+                                    <Pressable
+                                        style={styles.tickBox}
+                                        onPress={() => {
+                                            const newTasks = tasks.map((t) => {
+                                                if (t.id === task.id) {
+                                                    t.completed = true;
+                                                }
+                                                return t;
+                                            });
+                                            setTasks(newTasks);
+                                        }}
+                                    >
                                         <Text style={styles.box_text}>✅</Text>
                                     </Pressable>
                                 </View>
@@ -70,18 +89,18 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#1b1f24'
+        backgroundColor: '#1b1f24',
     },
     tasklist_container: {
         marginTop: 50,
-        width: "90%",
-        marginLeft: "5%",
+        width: '90%',
+        marginLeft: '5%',
     },
 
     tasklist_header: {
         borderRadius: 5,
-        width: "100%",
-        height: "5%",
+        width: '100%',
+        height: '5%',
         backgroundColor: '#455CA0',
         justifyContent: 'center',
         alignItems: 'center',
@@ -93,19 +112,19 @@ const styles = StyleSheet.create({
     },
 
     tasklist_body: {
-        height: "95%",
+        height: '95%',
     },
     task_container: {
         borderRadius: 5,
         backgroundColor: 'rgb(240, 240, 240)',
         justifyContent: 'center',
-        paddingHorizontal: "5%",
-        marginBottom: 10,     
+        paddingHorizontal: '5%',
+        marginBottom: 10,
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-        width: "100%",
-        height: "8%",
+        width: '100%',
+        height: '8%',
     },
     task_completed: {
         fontSize: 12,
@@ -128,7 +147,6 @@ const styles = StyleSheet.create({
         marginHorizontal: 5,
     },
 
-    
     button: {
         alignItems: 'center',
         justifyContent: 'center',
@@ -137,16 +155,15 @@ const styles = StyleSheet.create({
         borderRadius: 4,
         elevation: 3,
         backgroundColor: 'rgb(240, 240, 240)',
-        width: "60%",
-        marginLeft: "20%",
-        margin: 20
+        width: '60%',
+        marginLeft: '20%',
+        margin: 20,
     },
     text: {
-      fontSize: 12,
-      lineHeight: 21,
-      fontWeight: 'bold',
-      letterSpacing: 0.25,
-      color: 'black',
+        fontSize: 12,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: 'black',
     },
-
 });
--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # My First Mobile App (Built in 1 hour)
 
-## This is a React-Native app that I built in 1 hour. It is a simple todo list app. 
+## This is a React-Native app that I built in 1 hour. It is a simple todo list app.
 
 ## Add Task doesn't work :(
 
 ## In-case you can't run it, heres a gif of it:
+
 #![](app.gif)
